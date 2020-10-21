f4a30838c8d7d4ed64ded6dbdaf765154cb7b997
diegopagini <diegopagini777@gmail.com>
2020-10-21 06:58:11 -0300

    armando el header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <main>
+    <px-header />
+    <px-assets-table />
   </main>
 </template>
 
 <script>
+import PxHeader from "@/components/PxHeader";
+import PxAssetsTable from "@/components/PxAssetsTable";
 
 export default {
   name: "App",
-  components: {}
+  components: { PxHeader, PxAssetsTable },
 };
 </script>
 
